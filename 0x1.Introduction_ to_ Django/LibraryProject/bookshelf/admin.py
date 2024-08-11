99cfa02bec7f56e63466e8d33a36ac03ec2332d9
marouanelmail <elmail0.1marouan@gmail.com>
2024-08-11 17:57:48 +0100

    costumizing

--- 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
+++ 0x1.Introduction_ to_ Django/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin 
 from .models import Book
 
+class BookAdmin(admin.ModelAdmin):
+    # Customize the list display to show title, author, and published date
+    list_display = ('title', 'author', 'publication_year')
 
-admin.site.register(Book)
+    # Add list filters for author and published date
+    list_filter = ('author', 'publication_year')
+
+    # Enable search by title and author
+    search_fields = ('title', 'author')
+
+# Register the Book model with the custom BookAdmin configuration
+admin.site.register(Book, BookAdmin)
