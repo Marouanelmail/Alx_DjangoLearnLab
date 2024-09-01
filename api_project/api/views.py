0735dd5ef7ec44325eba8f33393e8c807fa712d5
marouanelmail <elmail0.1marouan@gmail.com>
2024-09-01 18:31:18 +0100

    apis

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -16,3 +16,12 @@ from .serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+from rest_framework import permissions, viewsets
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
