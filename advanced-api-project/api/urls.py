b3e7e0ba9d04322ed17ab8f0559b7906e7484678
marouanelmail <elmail0.1marouan@gmail.com>
2024-09-08 14:51:35 +0100

    apis

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from .views import BookListView, BookDetailView, BookCreateView, BookUpdateView,
 urlpatterns = [
     path('books/', BookListView.as_view(), name='book_list'),  # List view
     path('books/<int:pk>/', BookDetailView.as_view(), name='book_detail'),  # Detail view
-    path('books/add/', BookCreateView.as_view(), name='book_add'),  # Create view
-    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='book_edit'),  # Update view
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book_delete'),  # Delete view
+    path('books/add/', BookCreateView.as_view(), name='books/create'),  # Create view
+    path('books/<int:pk>/edit/', BookUpdateView.as_view(), name='books/update'),  # Update view
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='books/delete'),  # Delete view
 ]
