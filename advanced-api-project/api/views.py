5ebb06d428c5a6173f237b4702732fe8020750a1
marouanelmail <elmail0.1marouan@gmail.com>
2024-09-08 15:28:46 +0100

    apis

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -110,7 +110,7 @@ class BookDeleteView(LoginRequiredMixin, DeleteView):
 
 #######
 from rest_framework import generics
-from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as filters
 from rest_framework.filters import SearchFilter, OrderingFilter
 from .models import Book
 from .serializers import BookSerializer
@@ -120,7 +120,7 @@ class BookListView(generics.ListAPIView):
     """Handles listing all book instances with filtering, searching, and ordering"""
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    filter_backends = (DjangoFilterBackend, SearchFilter, OrderingFilter)
+    filter_backends = (filters.DjangoFilterBackend, SearchFilter, OrderingFilter)
     filterset_class = BookFilter
     search_fields = ['title', 'author__name']
     ordering_fields = ['title', 'publication_year']
