5aec4a4f3390e3956fc99d459bb0b6b366e7ea1b
marouanelmail <elmail0.1marouan@gmail.com>
2024-09-19 10:39:48 +0100

    django api

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
 User = get_user_model().objects.create_user
-
+serializers.CharField()
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
